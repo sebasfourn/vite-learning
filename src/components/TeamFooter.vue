4641fd9b882ea46f2c06e6b8d51109f374ab7d0f
Sébastien Fournier <sfournier.qc@gmail.com>
2023-12-13 15:59:03 -0500

    m

--- src/components/TeamFooter.vue
+++ src/components/TeamFooter.vue
@@ -1,12 +1,13 @@
 <script setup>
-    import { useTeamStore } from '@/stores/TeamStore'
+import { useTeamStore } from "@/stores/TeamStore";
 
-    let team = useTeamStore()
+let team = useTeamStore();
 </script>
 
 <template>
-    <footer class="mt-12 bg-gray-100 py-4 text-center">
-        <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
-    </footer>
+  <footer class="mt-12 bg-gray-100 py-4 text-center">
+    <h5 class="font-semibold text-lg">
+      {{ team.name }} - {{ team.spots }} Member Team
+    </h5>
+  </footer>
 </template>
-  
--- src/stores/TeamStore.js
+++ src/stores/TeamStore.js
@@ -1,26 +1,26 @@
-import { defineStore } from "pinia"
+import { defineStore } from "pinia";
 
-export let useTeamStore = defineStore('team', {
-    state: () => ({
-        name: '',
-        spots: 0,
-        members: []
-    }),
+export let useTeamStore = defineStore("team", {
+  state: () => ({
+    name: "",
+    spots: 0,
+    members: [],
+  }),
 
-    actions: {
-        async fill() {
-            let r = await import('@/team.json')
-            this.$state = r.default
-        },
-
-        grow(spots) {
-            this.spots = spots
-        }
+  actions: {
+    async fill() {
+      let r = await import("@/team.json");
+      this.$state = r.default;
     },
 
-    getters: {
-        spotsRemaining() {
-            return this.spots - this.members.length
-        }
-    }
-})
+    grow(spots) {
+      this.spots = spots;
+    },
+  },
+
+  getters: {
+    spotsRemaining() {
+      return this.spots - this.members.length;
+    },
+  },
+});
