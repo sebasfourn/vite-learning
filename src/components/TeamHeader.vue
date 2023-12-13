8b0f293531a201d155416a50711be768b8404f1e
Sébastien Fournier <sfournier.qc@gmail.com>
2023-12-12 20:51:03 -0500

    vite project

---
+++ src/components/TeamHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { useTeamStore } from '@/stores/TeamStore'
+	import AddMemberModal from "@/components/AddMemberModal.vue"
+
+    let team = useTeamStore()
+</script>
+
+<template>
+	<header class="flex justify-between">
+		<div>
+			<AddMemberModal />
+		</div>
+
+		<div>
+			<div class="inline-flex items-center text-3xl relative">
+				<img src="/favicon.ico" alt="" class="mr-2">
+				<h3>{{ team.name }} Team</h3>
+				<div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">{{ team.spots }}</div>
+			</div>
+		</div>
+ 	</header>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,20 @@
+<script setup>
+	import TeamHeader from "@/components/TeamHeader.vue"
+	import TeamMembers from "@/components/TeamMembers.vue"
+	import TeamFooter from "@/components/TeamFooter.vue"
+	import { useTeamStore } from "@/stores/TeamStore"
+
+	let team = useTeamStore()
+
+	team.fill()
+</script>
+
+<template>
+	<TeamHeader />
+  
+	<div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
+		<TeamMembers />
+	</div>
+  
+	<TeamFooter />
+</template>
